810c6de769f9c528d2bf8c8be335ebfb4cfa45a2
Jean Pierre <jeanp413@hotmail.com>
2023-02-22 15:27:43 -0500

    IExternalUriResolver can return URI or URL

--- src/vs/workbench/browser/web.api.ts
+++ src/vs/workbench/browser/web.api.ts
@@ -367,7 +367,7 @@ export interface ICommonTelemetryPropertiesResolver {
 }
 
 export interface IExternalUriResolver {
-	(uri: URI): Promise<URI>;
+	(uri: URI): Promise<URI | URL>;
 }
 
 export interface IExternalURLOpener {
